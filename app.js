657555b9b9710a4f6c55241414c4b2f0b85ab3a2
Sabria Islamx <sabria.islam@gmail.com>
2024-10-29 09:24:08 -0400

    express

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.use(express.static("public"));
+
 app.listen(port, () => {
   console.log(`Cool app listening on port ${port}`)
 })
