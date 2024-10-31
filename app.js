c06036a72b3222602483f0ab7d9e540bf1587127
Sabria Islamx <sabria.islam@gmail.com>
2024-10-31 09:22:43 -0400

    oct 31

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const indexRoute = require('./routes/index.js')
+const aboutRoute = require ('./routes/about.js')
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.use('/', indexRoute);
+app.use('/', aboutRoute);
 
 app.use(express.static("public"));
 
 app.listen(port, () => {
   console.log(`Cool app listening on port ${port}`)
 })
+
